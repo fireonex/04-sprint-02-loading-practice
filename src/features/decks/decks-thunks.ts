604d60c677874013eb3e80255b6d2cace64e4de8
fireonex <mari.krulikovskaya@mail.ru>
2024-07-05 17:00:21 +0300

    3rd commit (added error handling)

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,20 +2,21 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setAppStatusAC } from '../../app/app-reducer.ts'
+import axios, { Axios, AxiosError, isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => {
   return async (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC('loading'));
+    dispatch(setAppStatusAC('loading'))
 
     try {
-      const res = await decksAPI.fetchDecks();
-      dispatch(setDecksAC(res.data.items));
-      dispatch(setAppStatusAC('succeeded'));
+      const res = await decksAPI.fetchDecks()
+      dispatch(setDecksAC(res.data.items))
+      dispatch(setAppStatusAC('succeeded'))
     } catch (err) {
-      dispatch(setAppStatusAC('failed'));
+      dispatch(setAppStatusAC('failed'))
     }
-  };
-};
+  }
+}
 
 
 export const addDeckTC = (name: string) => async (dispatch: Dispatch) => {
@@ -30,8 +31,42 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
   })
 }
 
+// case-1: ошибки бэкенда (на стороне бэкенда). Ошибку создаёт axios, в e.response.data помещает ответ сервера
+// case-2: network error -- axios создаёт объект ошибки, сообщение можно взять из поля e.message
+// case-3: синхронные ошибки -- создаётся "нативная" JS-ошибка, имеет поле e.message
+
+//todo: вывести в консоль сообщение об ошибке учтя все кейсы
+// взять и достать из объекта ошибки сообщение об ошибке
+
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (err) {
+    let errorMessage: string
+    // if (axios.isAxiosError(err)) {
+    //   if (err.code === 'ERR_INTERNET_DISCONNECTED') {
+    //     errorMessage = 'No internet connection'
+    //     console.log(errorMessage)
+    //   } else if (err.response) {
+    //     errorMessage = err.response.data.errorMessages?.[0]?.message
+    //     console.log(errorMessage)
+    //   }
+    // } else {
+    //   errorMessage = 'something went wrong'
+    //   console.log(errorMessage)
+    // }
+
+    if (isAxiosError<ServerError>(err)) {
+      errorMessage = err.response ? err.response.data.errorMessages[0].message : err.message
+    } else {
+      errorMessage = (err as Error).message
+    }
+    console.log(errorMessage)
+  }
 }
+
+type ServerError = {
+  errorMessages: Array<{ field: string, message: string }>
+}
+
